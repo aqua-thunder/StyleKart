7ca67500fc503cfa600814934353946b9f6078f1
Ayush <gandhiayush291@gmail.com>
2026-01-12 15:27:00 +0530

    hamburger commit

--- client/src/pages/Hamburger.jsx
+++ client/src/pages/Hamburger.jsx
@@ -19,27 +19,27 @@ const Hamburger = (props) => {
     return (
         <div>
             <div className='w-0 h-0 left-0 overflow-x-hidden fixed bg-white z-20' id='hamburger'>
-                <div className='p-5' >
-                    <div className='flex justify-between items-center'>
+                <div className='' >
+                    <div className='flex justify-between items-center pt-7 px-8'>
                         <h1 className='text-center text-[21px]'>Account</h1>
                         <img onClick={FilterClose} className='bg-gray-900 rounded-full invert w-5 cursor-pointer' src="/images/SVG/cross.svg" alt="" />
                     </div>
-                    <ul className='  border border-gray-200 flex flex-col rounded-lg space-y-10 mt-5 py-7 pl-7 text-sm font-semibold '>
-                        <NavLink to="/men" onClick={FilterClose} className='cursor-pointer'>Men</NavLink>
-                        <NavLink to="/women" onClick={FilterClose} className='cursor-pointer'>Women</NavLink>
-                        <NavLink to="/kids" onClick={FilterClose} className='cursor-pointer'>Kids</NavLink>
-                        <NavLink to="/beauty" onClick={FilterClose} className='cursor-pointer'>Beauty</NavLink>
-                        <NavLink to="/genz" onClick={FilterClose} className='cursor-pointer'>Genz</NavLink>
-                        <NavLink to="/wishlist" onClick={FilterClose} className='cursor-pointer'>Wishlist</NavLink>
-                        <NavLink to="/cart" onClick={FilterClose} className='cursor-pointer'>Cart</NavLink>
-                        <NavLink to="/account" onClick={FilterClose} className='cursor-pointer'>Account</NavLink>
-                        <NavLink to="/orders" onClick={FilterClose} className='cursor-pointer'>Orders</NavLink>
-                        <NavLink to="/myntraMail" onClick={FilterClose} className='cursor-pointer'>Mall</NavLink>
-                        <NavLink to="/myntraInsiders" onClick={FilterClose} className='cursor-pointer'>Insiders</NavLink>
-                        <NavLink to="/contact" onClick={FilterClose} className='cursor-pointer'>Contactli</NavLink>
-                        <NavLink to="/FAQ" onClick={FilterClose} className='cursor-pointer'>FAQs</NavLink>
-                        <NavLink to="/legal" onClick={FilterClose} className='cursor-pointer'>Legal</NavLink>
-                        <NavLink to="" className='cursor-pointer'>Virtual Try On</NavLink>
+                    <ul className='  border border-gray-200 flex flex-col rounded-lg space-y-5 mt-5 py-7  text-sm font-semibold '>
+                        <NavLink to="/men" onClick={FilterClose} className='cursor-pointer border-b-2 border-gray-300 py-5 pl-8'>Men</NavLink> 
+                        <NavLink to="/women" onClick={FilterClose} className='cursor-pointer border-b-2 border-gray-300 py-5 pl-8'>Women</NavLink>
+                        <NavLink to="/kids" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Kids</NavLink>
+                        <NavLink to="/beauty" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Beauty</NavLink>
+                        <NavLink to="/genz" onClick={FilterClose} className='cursor-pointer border-b-2 border-gray-300 py-5 pl-8'>Genz</NavLink>
+                        <NavLink to="/wishlist" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Wishlist</NavLink>
+                        <NavLink to="/cart" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Cart</NavLink>
+                        <NavLink to="/account" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Account</NavLink>
+                        <NavLink to="/orders" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Orders</NavLink>
+                        <NavLink to="/myntraMail" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Mall</NavLink>
+                        <NavLink to="/myntraInsiders" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>Insiders</NavLink>
+                        <NavLink to="/contact" onClick={FilterClose} className='cursor-pointer border-b-2 border-gray-300 py-5 pl-8'>Contactli</NavLink>
+                        <NavLink to="/FAQ" onClick={FilterClose} className='cursor-pointer  border-b-2 border-gray-300 py-5 pl-8'>FAQs</NavLink>
+                        <NavLink to="/legal" onClick={FilterClose} className='cursor-pointer border-b-2 border-gray-300 py-5 pl-8'>Legal</NavLink>
+                        <NavLink to="" className='cursor-pointer border-b-2 border-gray-300 py-2 pl-4'>Virtual Try On</NavLink>
                     </ul>
                 </div>
             </div>
