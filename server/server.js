9351296901d466262eedc507f2936f23d98acd29
Ayush <gandhiayush291@gmail.com>
2026-01-17 18:48:45 +0530

    commit for render

--- server/server.js
+++ server/server.js
@@ -13,11 +13,7 @@ const cartRoute = require("./router/cart-router.js")
 const connectDb = require("./utils/db.js");
 const errorMiddleware = require("./middlewares/error-middleware.js");
 
-// const corsOptions = {
-//     origin: "http://localhost:5173",
-//     methods: "GET, POST , PUT , DELETE, PATCH, HEAD",
-//     credentials: true
-// }
+
 const corsOptions = {
     origin: [
         "http://localhost:5173",
@@ -36,6 +32,13 @@ app.use("/api/data", serviceRoute)
 app.use("/api/admin", adminRoute)
 app.use("/api/wishlist", wishlistRoute);
 app.use("/api/cart", cartRoute)
+
+// bellow api is for fast working and not to sleep the server on render
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+
 app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 7000;
